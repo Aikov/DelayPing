4982985bd352816aeda7d4bb28724432465bec6b
King Aikov <dzhlong@outlook.com>
2019-09-04 19:50:44 +0800

    little try still good yet

---
+++ Tool.py
@@ -0,0 +1,28 @@
+import csv
+from time import *
+
+import pythonping
+
+error_log = open('error_log.csv', 'a', newline='')
+eWriter = csv.writer(error_log)
+
+
+def ping_result(ip) -> str:
+    """
+
+    :param ip: could be a ip address or any web name
+    :return: a tuple return[0] is a flag of success a[1] is the time or error info
+    """
+    try:
+        a = pythonping.ping(target=ip, count=1)
+    except Exception as e:
+        data = [asctime(localtime(time())), e]
+        eWriter.writerow(data)
+        return 'Error check log file'
+    a = str(a)
+    pos1 = a.find('in ')
+    pos2 = a.find('ms')
+    if pos1 is -1:
+        return 'Timeout'
+    else:
+        return a[pos1:pos2]
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import pythonping
 
-a = pythonping.ping('192.168.0.1')
+a = pythonping.ping(target='192.168.0.1',count=1)
 print(a)
+print(type(a))
+a = str(a)
+print(a)
+print(type(a))
+pos1 = a.find('in ') + 3
+pos2 = a.find('ms')
+print(a[pos1:pos2])
