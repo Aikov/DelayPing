163a87832e34e1ab5e612ad38613ea8bdbf9e7cf
King Aikov <dzhlong@outlook.com>
2019-09-05 12:39:34 +0800

    keep trying I hate GUI again so far so good

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ import tkinter as tk
 from Tool import *
 
 InputWindow = tk.Tk()
-InputWindow.geometry('200x500')
+InputWindow.geometry('500x200')
 InputWindow.title('Input')
 InputWindow.configure(bg='#0084ff')
 EntryLocal = tk.Entry(InputWindow, bg='#e42bff')
+EntryLocal.place(x=60, y=40)
+string = tk.StringVar()
+string.set('<-input local ip here')
+LocalLabel = tk.Label(InputWindow, textvariable=string
+                      , bg='#0084ff', fg='black',
+                      font=('Arial', 12))
+LocalLabel.place(x=240, y=40)
+
 EntryCloud = tk.Entry(InputWindow, bg='#e43bff')
+InputWindow.mainloop()
 
 
 def my_exit():
