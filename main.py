635f58919012f508b8a208e55e46b6f506e44311
King Aikov <dzhlong@outlook.com>
2019-09-06 22:30:19 +0800

    keep trying Finish the button I hate GUI the third time so far so good

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ CloudLabel = tk.Label(InputWindow, textvariable=string_2
                       , bg='black', fg='yellow',
                       font=('Arial', 12))
 CloudLabel.place(x=80, y=80)
-InputWindow.mainloop()
 
 
 def my_exit():
@@ -38,6 +37,16 @@ def confirm():
     global Local_ip, Cloud_ip
     Local_ip = EntryLocal.get()
     Cloud_ip = EntryCloud.get()
+    InputWindow.destroy()
     return
 
+
+ConfirmButton = tk.Button(InputWindow, text='Confirm', bg='yellow', font=('Arial', 14), command=confirm, width=10,
+                          height=1)
+ConfirmButton.place(x=90, y=120)
+
+ExitButton = tk.Button(InputWindow, text='Exit', bg='yellow', font=('Arial', 14), command=my_exit, width=10,
+                       height=1)
+ExitButton.place(x=310, y=120)
+InputWindow.mainloop()
 # TODO:Finish the button
