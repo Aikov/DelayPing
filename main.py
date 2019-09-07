9ceb26b43de82ac05d2b7dac702e8c30a45edcd0
King Aikov <dzhlong@outlook.com>
2019-09-07 15:49:56 +0800

    keep trying Start build second window so far so good

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import tkinter as tk
 
 from Tool import *
@@ -12,8 +13,8 @@ EntryLocal.place(x=80, y=40)
 
 string = tk.StringVar()
 string.set('<-input local ip here')
-LocalLabel = tk.Label(InputWindow, textvariable=string
-                      , bg='black', fg='yellow',
+LocalLabel = tk.Label(InputWindow, textvariable=string,
+                      bg='black', fg='yellow',
                       font=('Arial', 12))
 LocalLabel.place(x=300, y=40)
 
@@ -22,8 +23,8 @@ EntryCloud.place(x=300, y=80)
 
 string_2 = tk.StringVar()
 string_2.set('input cloud ip here->')
-CloudLabel = tk.Label(InputWindow, textvariable=string_2
-                      , bg='black', fg='yellow',
+CloudLabel = tk.Label(InputWindow, textvariable=string_2,
+                      bg='black', fg='yellow',
                       font=('Arial', 12))
 CloudLabel.place(x=80, y=80)
 
@@ -49,4 +50,44 @@ ExitButton = tk.Button(InputWindow, text='Exit', bg='yellow', font=('Arial', 14)
                        height=1)
 ExitButton.place(x=310, y=120)
 InputWindow.mainloop()
-# TODO:Finish the button
+
+Window = tk.Tk()
+Window.configure(bg='black')
+Window.geometry('500x200')
+Window.title('Main')
+string = tk.StringVar()
+string_2 = tk.StringVar()
+string_3 = tk.StringVar()
+
+
+def calc():
+    global Local_ip, Cloud_ip
+    body = ping_result(Local_ip)
+    delay1 = ''
+    delay2 = ''
+    if body[0]:
+        delay1 = body[1]
+        body = ping_result(Cloud_ip)
+        if body[0]:
+            delay2 = body[1]
+        else:
+            string_2.set('Error')
+            time.sleep(5)
+            my_exit()
+    else:
+        string.set('Error')
+        time.sleep(5)
+        my_exit()
+    delta_delay = float(delay2) - float(delay1)
+    if delta_delay < 0:
+        calc()
+    else:
+        string.set(delay1)
+        string_2.set(delay2)
+        string_3.set(str(delta_delay))
+
+
+LocalLabel = tk.Label(master=Window, textvariable=string, bg='yellow', font=('Arial', 12), fg='black')
+CloudLabel = tk.Label(master=Window, textvariable=string_2, bg='yellow', font=('Arial', 12), fg='black')
+DeltaLabel = tk.Label(master=Window, textvariable=string_3, bg='yellow', font=('Arial', 12), fg='black')
+Window.mainloop()
